f1b4c800f0ff3df62e61876c4efe2ca276c57021
Muslim.Chochlov <Muslim.Chochlov@9ZCDRF2.woodgroup.com>
2018-05-08 11:12:19 +0100

    #10517 Implement IR searching part

--- src/main/java/com/woodplc/cora/ir/LuceneIREngineWrapper.java
+++ src/main/java/com/woodplc/cora/ir/LuceneIREngineWrapper.java
@@ -3,6 +3,8 @@ package com.woodplc.cora.ir;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -10,11 +12,19 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.custom.CustomAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.SearcherManager;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
@@ -23,9 +33,12 @@ import com.woodplc.cora.app.Main.Resource;
 final class LuceneIREngineWrapper implements IREngine {
 	
 	private final static String INDEX_ROOT = "index";
+	private final static int MAX_HITS = Integer.MAX_VALUE;
 	private final Path indexPath;
 	private final Analyzer fortranAnalyzer;
 	private final IndexWriter writer;
+	private final SearcherManager searchManager;
+	private final QueryParser dataFieldQueryParser;
 	
 	private enum Fields{
 		NAME,
@@ -37,9 +50,8 @@ final class LuceneIREngineWrapper implements IREngine {
 		
 		try {
 			this.fortranAnalyzer = CustomAnalyzer.builder()
-					.addCharFilter("patternreplace", "pattern", "(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])", "replacement", " ")
+					//.addCharFilter("patternreplace", "pattern", "(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])", "replacement", " ")
 					.withTokenizer("lowercase")
-		
 					.addTokenFilter("stop", "ignoreCase", "true", "words", Resource.FORTRAN_KEYWORDS.path(), "format", "snowball")
 					.addTokenFilter("porterstem")
 					.addTokenFilter("stop", "ignoreCase", "true")//, "words", "stopwords.txt", "format", "wordset")
@@ -47,8 +59,10 @@ final class LuceneIREngineWrapper implements IREngine {
 					.build();
 			
 			Directory dir = FSDirectory.open(indexPath);
-			IndexWriterConfig iwc = new IndexWriterConfig(fortranAnalyzer).setOpenMode(OpenMode.CREATE_OR_APPEND);
+			IndexWriterConfig iwc = new IndexWriterConfig(fortranAnalyzer).setOpenMode(OpenMode.CREATE);
 			this.writer = new IndexWriter(dir, iwc);
+			this.searchManager = new SearcherManager(writer, true, true, null);
+			this.dataFieldQueryParser = new QueryParser(Fields.DATA.name(), fortranAnalyzer);
 
 		} catch (IOException e) {
 			throw new IllegalStateException(e.getMessage());
@@ -64,7 +78,13 @@ final class LuceneIREngineWrapper implements IREngine {
 		
 		Document doc = new Document();
 		doc.add(new StringField(Fields.NAME.name(), subname, Field.Store.YES));
-		doc.add(new TextField(Fields.DATA.name(), textData, Field.Store.NO));
+		FieldType customType = new FieldType();
+		customType.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS);
+		customType.setTokenized(true);
+		customType.setStoreTermVectors(true);
+		customType.freeze();
+		Field textField = new Field(Fields.DATA.name(), textData, customType);
+		doc.add(textField);
 		try {
 			writer.addDocument(doc);
 		} catch (IOException e) {
@@ -77,6 +97,7 @@ final class LuceneIREngineWrapper implements IREngine {
 		if (writer != null && writer.hasUncommittedChanges()) {
 			try {
 				writer.commit();
+				searchManager.maybeRefreshBlocking();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -84,20 +105,45 @@ final class LuceneIREngineWrapper implements IREngine {
 	}
 
 	@Override
-	public List<String> search(String query) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void close() {
-		if (writer != null && writer.isOpen()) {
+	public List<String> search(String queryString) {
+		if (queryString == null || queryString.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		
+		IndexSearcher searcher = null;
+		try {
+			searcher = searchManager.acquire();
+			Query query = dataFieldQueryParser.parse(queryString);
+			
+			ScoreDoc[] hits = searcher.search(query, MAX_HITS).scoreDocs;
+			
+			List<String> matchingSubprograms = new ArrayList<>();
+			for (ScoreDoc hit : hits) {
+				matchingSubprograms.add(searcher.doc(hit.doc).get(Fields.NAME.name()));
+			}
+			return matchingSubprograms;
+			
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+			return Collections.emptyList();
+		} finally {
 			try {
-				writer.close();
+				searchManager.release(searcher);
+				searcher = null;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 	
+	@Override
+	public void close() {
+		try {
+			if (writer != null && writer.isOpen()) {writer.close();}	
+			if (searchManager != null) {searchManager.close();}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
