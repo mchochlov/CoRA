c35d6a3173d449a11cd131fbff9cab21a8e48478
Muslim.Chochlov <Muslim.Chochlov@9ZCDRF2.woodgroup.com>
2018-06-18 20:29:34 +0100

    #12266 Load/save state from/to json

--- src/main/java/com/woodplc/cora/data/Feature.java
+++ src/main/java/com/woodplc/cora/data/Feature.java
@@ -1,27 +1,40 @@
 package com.woodplc.cora.data;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public final class Feature {
 	
-	private final ObservableList<String> systemASubprograms;
-	private final ObservableList<String> systemBSubprograms;
-	private final ObservableList<String> systemCSubprograms;
+	private final ObservableList<String> systemASubprograms = FXCollections.observableArrayList();
+	private final ObservableList<String> systemBSubprograms = FXCollections.observableArrayList();
+	private final ObservableList<String> systemCSubprograms = FXCollections.observableArrayList();
 	
-	private Feature() {
-		systemASubprograms = FXCollections.observableArrayList();
-		systemBSubprograms = FXCollections.observableArrayList();
-		systemCSubprograms = FXCollections.observableArrayList();
+	public Feature() {}
+
+	public Feature(List<String> subprogramsA, 
+			List<String> subprogramsB, 
+			List<String> subprogramsC) {
+		Objects.requireNonNull(subprogramsA);
+		Objects.requireNonNull(subprogramsB);
+		Objects.requireNonNull(subprogramsC);
+		if (!subprogramsA.isEmpty()) this.systemASubprograms.addAll(subprogramsA);
+		if (!subprogramsB.isEmpty()) this.systemBSubprograms.addAll(subprogramsB);
+		if (!subprogramsC.isEmpty()) this.systemCSubprograms.addAll(subprogramsC);
 	}
 	
-	public static Feature newBlankFeature() {
-		return new Feature();
-	}
-	
-	public ObservableList<String> systemASubprograms(){return systemASubprograms;}
+	public ObservableList<String> systemASubprograms() {return systemASubprograms;}
 
 	public ObservableList<String> systemBSubprograms() {return systemBSubprograms;}
 	
 	public ObservableList<String> systemCSubprograms() {return systemCSubprograms;}
+	
+	public List<String> readOnlySystemASubprograms() {return Collections.unmodifiableList(systemASubprograms);}
+	
+	public List<String> readOnlySystemBSubprograms() {return Collections.unmodifiableList(systemBSubprograms);}
+	
+	public List<String> readOnlySystemCSubprograms() {return Collections.unmodifiableList(systemCSubprograms);}
 }
--- src/test/java/com/woodplc/cora/utils/TestUtils.java
+++ src/test/java/com/woodplc/cora/utils/TestUtils.java
@@ -1,5 +1,6 @@
 package com.woodplc.cora.utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,10 +8,17 @@ import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.stream.Stream;
 
+import com.woodplc.cora.data.ApplicationState;
+import com.woodplc.cora.data.Feature;
 import com.woodplc.cora.data.Graphs;
+import com.woodplc.cora.data.ModuleContainer;
 import com.woodplc.cora.data.SDGraph;
+import com.woodplc.cora.gui.model.EntityView;
 import com.woodplc.cora.parser.Parser;
 import com.woodplc.cora.parser.Parsers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public final class TestUtils {
 
@@ -41,5 +49,30 @@ public final class TestUtils {
 				.forEach(p -> graph.merge(parser.parse(p)));
 			return graph;
 	    }
+	}
+
+	public static ApplicationState emptyApplicationState() {
+		return new ApplicationState(null, null, FXCollections.observableArrayList(), 
+				ModuleContainer.empty(), ModuleContainer.empty(), ModuleContainer.empty(), 
+				new Feature());
+	}
+
+	public static ApplicationState fullyInitializedApplicationState() {
+		File lastKnownDir = new File(SoftwareSystem.TEST.path.toString());
+		String searchQuery = "searchQuery";
+		ObservableList<EntityView> searchResults = FXCollections.observableArrayList(
+				new EntityView(1, "subprogram_1"),
+				new EntityView(2, "subprogram_2"),
+				new EntityView(3, "subprogram_3")
+				);
+		ModuleContainer mc1 = ModuleContainer.fromValues(SoftwareSystem.FLEX3.path.toString(), "checksum_1");
+		ModuleContainer mc2 = ModuleContainer.fromValues(SoftwareSystem.DPRFLEX3.path.toString(), "checksum_2");
+		ModuleContainer mc3 = ModuleContainer.fromValues(SoftwareSystem.MAM.path.toString(), "checksum_3");
+		Feature feature = new Feature();
+		feature.systemASubprograms().addAll("subprogram_a", "subprogram_b", "subprogram_c");
+		feature.systemBSubprograms().addAll("subprogram_d", "subprogram_e");
+		feature.systemCSubprograms().addAll("subprogram_f");
+		return new ApplicationState(lastKnownDir, searchQuery, searchResults, 
+				mc1, mc2, mc3, feature);
 	}
 }
